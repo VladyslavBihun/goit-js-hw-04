50d8d27a0c4cf9d2bd6389d12a93ebca1545cd75
VladyslavBihun <bihunvv@gmail.com>
2024-02-17 19:50:53 +0200

    first task has done

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,26 @@
-function slugify(title) {
-    const titleArray = title.toLowerCase().split(" ");
-    const slugTitle = titleArray.join("-");
-    return slugTitle;
-}
+function isEnoughCapacity(products, containerSize) {
+    let totalCountProducts = 0;
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+    for (const key in products) {
+        totalCountProducts += products[key];
+    }
+    if (totalCountProducts <= containerSize) {
+        return true;
+    }
+    return false;
+}
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
